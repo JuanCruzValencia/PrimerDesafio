407fe0b84392f8678a27a66ebed57757ba83e9a6
Juan Cruz Valencia Vernengo <jcvalenciaver@gmail.com>
2023-09-07 10:16:29 -0300

    Update User.cs

--- PrimerDesafio/User.cs
+++ PrimerDesafio/User.cs
@@ -20,7 +20,7 @@ namespace PrimerDesafio
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
+        private string Password { get; set; }
         public string UserName { get; set; }
     }
 }
